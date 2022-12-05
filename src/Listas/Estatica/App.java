61a5d74814ecb3a3c53f08f9689d8d354d3dbb1d
Uriel Galindo <ugalu448@gmail.com>
2022-12-05 15:31:30 -0600

    Estructuras lineales terminadas

--- src/Listas/Estatica/App.java
+++ src/Listas/Estatica/App.java
@@ -1,5 +1,34 @@
 package Listas.Estatica;
 
+import java.util.Scanner;
+
 public class App {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Lista lista;
+        int length;
+
+        System.out.print("Digite el tamaño de la lista: ");
+        length = input.nextInt();
+
+        lista = new Lista(length);
+
+        for (int i = 0; i < 5; i++) {
+            lista.input(i);
+        }
+
+        for (int i = 10; i > 5; i--) {
+            lista.input(i);
+        }
+
+        // for (int i = 0; i < 10; i++) {
+        //     lista.output(i);
+        // }
+        lista.output(4);
+        lista.output(6);
+        lista.output(5);
+        // lista.mostrar();
+
+        input.close();
+    }
 }
--- src/Listas/Estatica/Lista.java
+++ src/Listas/Estatica/Lista.java
@@ -1,5 +1,127 @@
 package Listas.Estatica;
 
+import javax.swing.text.Position;
+
 public class Lista {
+    private int lista[];
+    private int start, end, totalDatos;
+
+    public void input(int dato){
+        int posicion;
+
+        if(totalDatos == lista.length){
+            System.out.println("Overflow");
+        }
+        else if(totalDatos == 0) {
+            start = end = 0;
+            lista[start] = dato;
+            totalDatos++;
+        }
+        else if(lista[start] > dato){
+            realizarCorrmientos();
+            lista[start] = dato;
+            end++;
+            totalDatos++;
+        }
+        else if(lista[end] < dato){
+            end++;
+            lista[end] = dato;
+            totalDatos++;
+        }
+        else{
+            posicion = buscarPosicion(dato) + 1;
+            realizarCorrmientos((totalDatos - posicion));
+            lista[posicion] = dato;
+            totalDatos++;
+            end++;
+        }
+    }
+
+    private void realizarCorrmientos(){
+        int index = totalDatos - 1;
+
+        while(index >= 0){
+            lista[index + 1] = lista[index];
+            index--;
+        }
+    }
     
+    private int buscarPosicion(int dato){
+        int index = 0;
+
+        while(index < totalDatos && lista[index + 1] < dato){
+            index++;
+        }
+
+        return index;
+    }
+
+    private void realizarCorrmientos(int corrimientosRealizar){
+        int index = totalDatos - 1;
+        int corrmientosRealizados = 0;
+
+        while (corrmientosRealizados <= corrimientosRealizar) {
+            lista[index + 1] = lista[index];
+            corrmientosRealizados++;
+            index--;
+        }
+
+    }
+
+    public void output(int dato){
+        int posicion, corrimientosRealizar;
+        if(totalDatos == 0){
+            System.out.println("Underflow");
+        }
+        else if(lista[start] == dato){
+            System.out.println(lista[start]);
+            start++;
+        }
+        else if(lista[end] == dato){
+            System.out.println(lista[end]);
+            end--;
+        }
+        else{
+            posicion = buscarPosicion(dato) + 1;
+
+            if(lista[posicion] == dato){
+
+                System.out.println(lista[posicion]);
+    
+                corrimientosRealizar = totalDatos - (posicion + 1);
+                realizarCorrmientosOutput(corrimientosRealizar, posicion);
+                totalDatos--;
+                end--;
+            }
+            else{
+                System.out.println("El dato " + dato + " no se encuentra en la lista");
+            }
+        }
+    }
+
+    private void realizarCorrmientosOutput(int corrimientosRealizar, int posicion){
+        int index = posicion;
+        int corrmientosRealizados = 0, indexCambiar = index + 1;
+
+        while (corrmientosRealizados <= corrimientosRealizar) {
+            if(indexCambiar < totalDatos){
+                lista[index] = lista[indexCambiar];
+            }
+            index++;
+            indexCambiar = index;
+            corrmientosRealizados++;
+        }
+    }
+
+    public void mostrar(){
+
+        for (int i = 0; i < totalDatos; i++) {
+            System.out.println(lista[i]);
+        }
+    }
+
+    Lista(int length){
+        lista = new int[length];
+        totalDatos = 0;
+    }
 }
